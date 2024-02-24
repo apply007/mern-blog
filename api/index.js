8b790dfd7e31f2adbe7fbe3b6a76210b6ee1a1a4
apply007 <hasanat153@gmail.com>
2024-02-25 02:07:08 +0600

    connect mongodb

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import mongoose from 'mongoose'
 const app=express()
 
 
+
+dotenv.config()
+
+mongoose.connect(process.env.DBCON)
+.then(()=>{
+    console.log("MongoDB is Connected")
+}).catch((err)=>{
+    console.log(err)
+})
+
+
+//
 app.listen(process.env.PORT,(req,res)=>{
 console.log("Server is ready and running at port 3000!!!")
 
