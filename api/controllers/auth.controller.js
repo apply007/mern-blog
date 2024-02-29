51f5afb96046aecd62e35bfdb0fcf784d233a7e7
apply007 <hasanat153@gmail.com>
2024-02-29 18:56:27 +0600

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import UserModel from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import handleError from "../utils/error.js";
-const signup = async (req, res,next) => {
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res, next) => {
   try {
     console.log(req.body);
 
@@ -14,16 +16,44 @@ const signup = async (req, res,next) => {
       email === "" ||
       password === ""
     ) {
-      next(handleError( 400,"All fields are required"))
+      next(handleError(400, "All fields are required"));
     }
-const hashPassword= bcryptjs.hashSync(password,10)
-    const newUser = new UserModel({ username, email,password:hashPassword });
+    const hashPassword = bcryptjs.hashSync(password, 10);
+    const newUser = await UserModel({
+      username,
+      email,
+      password: hashPassword,
+    });
 
     await newUser.save();
     res.status(200).json({ message: "Successfully save to MongoDB" });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
-export default signup;
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = await req.body;
+    if (!email || !password || email === "" || password === "") {
+      next(handleError(400, "All fields are required"));
+    }
+    const user = await UserModel.findOne({ email });
+    if (user === null) {
+     return next(handleError(404, "User Not Found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, user.password);
+    if (!validPassword) {
+     return next(handleError(400, "Wrong credential"));
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const {password: pass, ...rest}=user._doc
+    res.status(200).cookie("access_token", token,{httpOnly:true}).json(rest);
+  
+
+  
+  } catch (error) {
+    next(error);
+  }
+};
